fae6ea384405c2936b576e2576b04ed51928845d
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:46:10 +0200

    tb_rhs_savingday.chernarus mission added

---
+++ briefing_blue.hpp
@@ -0,0 +1,20 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <RP> Rally Points<br />
+<br />
+
+<titleFont>Deployment Options:</titleFont><br />
+<boxes> Forward Base<br />
+<FO> Forward Outpost<br />
+<RP> Team Rally Point<br />
+<HO> Hideout<br />
+<br />
+
+<titleFont><yellow>Objective</yellow></titleFont><br />
+<*>One of our Ah crashed in the north of Chernarus, a pilot survived<br />
+<*> You need to retrieve him and protect him<br />
+<*> <green>Your side will win if the ennemy has no more tickets or if the game time is over</green><br />
+<*> <red>Your side will loose if the VIP get unconsious and respawn</red><br />
+<*> Good luck guys, HOOWAH!<br />
+<br />
+
---
+++ briefing_red.hpp
@@ -0,0 +1,23 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <RP> Rally Points<br />
+<*> Help out your team by deploying <FO> Forward Outposts<br />
+<br />
+
+<titleFont>Deployment Options:</titleFont><br />
+<boxes> Forward Base<br />
+<FO> Forward Outpost<br />
+<RP> Team Rally Point<br />
+<HO> Hideout<br />
+<br />
+
+<titleFont><yellow>Objective</yellow></titleFont><br />
+<*>We downed one of their damn heli, but the pilot survived<br />
+<*> You need to retrieve him and kill him<br />
+<*> <green>Your side will win if the VIP get unconsious and respawn</green><br />
+<*> <red>Your side will loose if you have no more tickets or if the game time is over</red><br />
+<*> Good luck my brothers!<br />
+<br />
+
+
+
